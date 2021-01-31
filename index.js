85c7f693b1e6feabeb48feb435ffd29c8d23ac19
Daniel Sasse <dsasse07@gmail.com>
2021-01-30 20:21:04 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.match(/\d*/)
+  let leftInt = parseInt(leftNumbers)
+
+  if (leftInt > 0) {
+    dodger.style.left = `${leftInt - 1}px`
+  }
+}
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.match(/\d*/)
+  let leftInt = parseInt(leftNumbers)
+
+  if (leftInt < 360) {
+    dodger.style.left = `${leftInt + 1}px`
+  }
+}
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft()
+  }
+})
+
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight()
+  }
+})
